b5eba2cbd21540258f26c9c7cda817fe335d4f15
shaoyuyoung <2531116592@qq.com>
2022-04-08 21:29:29 +0800

    111

--- src/api/classrooms.js
+++ src/api/classrooms.js
@@ -2,27 +2,27 @@ export default {
   ownClassroom: [{
     id: 6,
     subject: '大学英语',
-    date: '2020-03-02',
-    end: '2024-03-02',
+    date: '2022-03-02',
+    end: '2022-12-31',
     name: '软工202'
   }, {
     id: 5,
     subject: '大学英语',
     date: '2021-03-02',
     end: '2025-03-02',
-    name: '软嵌201'
+    name: '软工201'
   }, {
     id: 2,
     subject: '专业英语',
     date: '2022-03-02',
     end: '2024-03-02',
-    name: '经管202'
+    name: '经管201'
   }, {
     id: 7,
     subject: '大学英语',
     date: '2019-03-02',
     end: '2023-03-02',
-    name: '大数据202'
+    name: '经管202'
   }, {
     id: 3,
     subject: '专业英语',
