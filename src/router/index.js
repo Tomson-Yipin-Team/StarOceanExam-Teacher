b00d7ffc4bf29cb939781963867249bcf098fafe
Zijin <2030110765@stmail.ntu.edu.cn>
2022-04-09 16:25:54 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -78,13 +78,13 @@ export const constantRoutes = [
         path: 'release',
         name: 'Release',
         component: () => import('@/views/examination/release/index'),
-        meta: { title: '发布考试', icon: 'edit' }
+        meta: { title: '发布考试', icon: 'el-icon-upload2' }
       },
       {
         path: 'paper',
         name: 'Paper',
         component: () => import('@/views/examination/paper/index'),
-        meta: { title: '卷库', icon: 'edit' }
+        meta: { title: '卷库', icon: 'el-icon-s-management' }
       },
       {
         path: 'create',
@@ -119,7 +119,7 @@ export const constantRoutes = [
       path: 'question-database',
       name: 'QuestionDB',
       component: () => import('@/views/question/timuku/index.vue'),
-      meta: { title: '题库', icon: 'edit' }
+      meta: { title: '题库', icon: 'el-icon-tickets' }
     }, {
       path: 'create-question',
       name: 'CreateQuestion',
