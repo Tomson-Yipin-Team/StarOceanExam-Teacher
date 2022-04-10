24d886d4b21150e230eca47a48987e692702b840
Zerolouis <wxjjuju0322@gmail.com>
2022-04-10 18:01:15 +0800

    修改批阅

--- src/api/correct-content.js
+++ src/api/correct-content.js
@@ -28,7 +28,7 @@ export default {
   }, {
     questionId: 1,
     category: 'translation', // 题目类型
-    content: `23123123131231231231`, // 题目
-    trueAnswer: `2` // 参考答案
+    content: `灯笼起源于东汉，最初主要用于照明。在唐代，人们用红灯笼来庆祝安定的生活。从那时起，灯笼在中国的许多地方流行起来。灯笼通常用色彩鲜艳的薄纸，形状和尺寸各异。在中国传统文化中，红灯笼象征生活美满和生意兴隆，通常在春节、元宵节和国庆等节日期间悬挂。如今，世界上许多其他地方也能看到红灯笼。`, // 题目
+    trueAnswer: `Lanterns originated in the Eastern Han Dynasty, and were at first used primarily for lighting. In the Tang Dynasty, people used red lanterns to celebrate a stable life. Since then, lanterns have became popular in many parts of China. Lanterns are usually made of brightly-colored tissue paper in a variety of shapes and sizes. In traditional Chinese culture, red lanterns symbolize a happy life and flourishing business, usually hung during festivals like the Spring Festival, Lantern Festival and National Day. Today, red lanterns can be seen in many other parts of the world.` // 参考答案
   }]
 }
