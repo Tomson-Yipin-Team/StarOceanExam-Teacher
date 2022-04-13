6d0631cdf053a8fd59470c13084132e0f42daf7c
Zerolouis <wxjjuju0322@gmail.com>
2022-04-13 13:50:39 +0800

    删除了右上角的消息

--- src/api/correct-content.js
+++ src/api/correct-content.js
@@ -24,7 +24,11 @@ export default {
     questionId: 0,
     category: 'writing',
     content: `123123123123123123123123`,
-    trueAnswer: `1`
+    trueAnswer: `A two-day visit to Fanhai Hope Elementary School organized by the Student Union of our university has been completed as scheduled this weekend.
+
+Aiming at fostering college students' service awareness and helping pupils of rural areas better understand the colorful college life so as to motivate them to study harder, the Student Union organized an activity named A Trip of Love Seed. A team of 30 college students' participated in this trip. The visit was warmly welcomed by all the students and staff of Fanhai, followed by a range of colorful activities, such as the interaction among students about the fascinating college life, a visit to the school art show of students' works of painting and handicraft and teaching activities focusing on showing the magical science world to the pupils.
+
+Both the pupils and our college students were actively involved in all these brilliant activities. And the headmaster of Fanhai spoke highly of this trip as well as the devotion, enthusiasm and creativity of our students.`
   }, {
     questionId: 1,
     category: 'translation', // 题目类型
