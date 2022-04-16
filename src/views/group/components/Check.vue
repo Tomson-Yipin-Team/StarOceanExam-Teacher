e35b7861e4939d0821758e66d0c3991866726b72
zerolouis <wxjjuju0322@gmail.com>
2022-04-16 21:46:20 +0800

    添加审阅试卷

--- src/views/group/components/Check.vue
+++ src/views/group/components/Check.vue
@@ -1,12 +1,60 @@
 <template>
   <div>
-    审阅
+    <el-table
+      :data="tableData"
+      style="width:100%"
+    >
+      <el-table-column
+        prop="id"
+        label="试卷ID"
+        width="200"
+      />
+      <el-table-column
+        prop="name"
+        label="试卷名称"
+      />
+      <el-table-column
+        prop="origin"
+        label="发卷人"
+      />
+      <el-table-column
+        #default="scope"
+        label="状态"
+      >
+        <el-tag type="danger">
+          {{ scope.row.status }}
+        </el-tag>
+      </el-table-column>
+      <el-table-column label="查看信息" width="300" fixed="right">
+        <el-button-group>
+          <el-button>试卷</el-button>
+          <el-button>发布信息</el-button>
+        </el-button-group>
+      </el-table-column>
+      <el-table-column label="审阅操作" width="300" fixed="right">
+        <el-button-group>
+          <el-button type="danger" plain>打回</el-button>
+          <el-button type="primary" plain>批准</el-button>
+        </el-button-group>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Check'
+  name: 'Check',
+  data() {
+    return {
+      tableData: [{
+        id: '123123123123123',
+        name: '四级模拟卷',
+        origin: '方汇民',
+        time: '2022-3-12',
+        status: '等待审核'
+      }]
+    }
+  }
 }
 </script>
 
--- src/views/group/index.vue
+++ src/views/group/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <el-card style="margin: 20px">
-      <el-tabs>
-        <el-tab-pane label="已加入">
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="已加入" name="joined">
           <Joined />
         </el-tab-pane>
-        <el-tab-pane label="我管理的">
+        <el-tab-pane label="我管理的" name="manager">
           <Manage />
         </el-tab-pane>
-        <el-tab-pane label="审批试卷">
+        <el-tab-pane label="审批试卷" name="check">
           <Check />
         </el-tab-pane>
       </el-tabs>
-      <create-group />
+      <create-group v-if="activeName!=='check'" />
     </el-card>
   </div>
 </template>
@@ -30,6 +30,11 @@ export default {
     Manage,
     CreateGroup,
     Check
+  },
+  data() {
+    return {
+      activeName: 'joined'
+    }
   }
 }
 </script>
